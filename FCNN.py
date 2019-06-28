5a38428c6c7332496cd2bf96707492a2d0853cfe
Le Huy <huykingsofm@gmail.com>
2019-06-29 01:26:39 +0700

    Utils for Neutral Network

---
+++ FCNN.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class FCNN(nn.Module):
+    VALID_LAYERS = ["sigmoid", "tanh", "relu", "softmax", "batchnorm", "dropout"]
+    def __init__(self, network: list):
+        super(FCNN, self).__init__()
+        for i in range(len(network)):
+            if isinstance(network[i], int) is False and isinstance(network[i], str) is True:
+                network[i] = (network[i], )
+
+        self.network = network
+        self.main = nn.Sequential()
+        last_layer_size = 0
+
+        for i, layer in enumerate(network):
+            if isinstance(layer, int):
+                layer_size = layer
+                if last_layer_size is not 0:
+                    self.main.add_module(str(i), nn.Linear(last_layer_size, layer_size, bias= True))
+                last_layer_size = layer_size
+            
+            if isinstance(layer, tuple):
+                kind_of_layer = layer[0]
+                if kind_of_layer not in FCNN.VALID_LAYERS:
+                    raise Exception("Kind of layer({}) is not invalid".format(kind_of_layer))
+
+                if kind_of_layer == "sigmoid":
+                    module = nn.Sigmoid()
+                elif kind_of_layer == "tanh":
+                    module = nn.Tanh()
+                elif kind_of_layer == "softmax":
+                    module = nn.Softmax(dim= -1)
+                elif kind_of_layer == "relu":
+                    module = nn.ReLU(inplace= True)
+                elif kind_of_layer == "batchnorm":
+                    module = nn.BatchNorm1d(last_layer_size)
+                elif kind_of_layer == "dropout":
+                    p = layer[1] if len(layer) == 2 else 0.5
+                    module = nn.Dropout(p= p, inplace= True)
+                self.main.add_module(str(i), module)
+    def forward(self, X):
+        return self.main(X)
+
+    def super_simple_graph(self):
+        graph = []
+        for i in range(len(self.network)):
+            if isinstance(self.network[i], int):
+                layer = str(self.network[i])
+            else:
+                layer = self.network[i][0]
+            graph.append(layer)
+            
+        return "->".join(graph)
+
+    
+if __name__ == "__main__":
+    network = [1, 2, 3, 4, "batchnorm", 5]
+    model = FCNN(network)
+    
---
+++ NNUtils.py
@@ -0,0 +1,105 @@
+import time
+import datetime
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def start_training(model: nn.Module, optimizer: optim, criterion, training_set:tuple, testing_set:tuple= None,
+     batch_size= 64, n_epoches= 10, checkpoint_att:tuple= None, print_att:tuple= None, history_att:tuple= "epoch"):
+    """
+    Start training a model with some attributes  
+    Args:
+        model         : A instance of torch.nn.Module
+        optimizer     : A instance of torch.optim
+        criterion     : A loss function which use for that model
+        training_set  : A tuple (training_input, training_output)
+        testing_set   : A tuple (testing_input, testing_output)
+        batch_size    : A int which is the size of each batches
+        n_epoches     : A int which is a number of iteration through over training set
+        checkpoint_att: A tuple (save_every: int, DIR)
+        print_att     : A tuple ("epoch" or "batch", print_every)
+        history_att   : "epoch" or "batch"
+    """
+    
+    all_start = time.time()
+    hist_loss = []
+
+    X = training_set[0]
+    Y = training_set[1]
+
+    if checkpoint_att != None:
+        if len(checkpoint_att) == 1:
+            if isinstance(checkpoint_att[0], int):
+                checkpoint_att = (checkpoint_att[0], "./")
+            elif isinstance(checkpoint_att[0], str):
+                checkpoint_att = (1, checkpoint_att[0])                
+                try:  
+                    os.mkdir(checkpoint_att[1])
+                except OSError:  
+                    print ("Creation of the directory %s failed" % checkpoint_att[1])
+                else:  
+                    print ("Successfully created the directory %s " % checkpoint_att[1])
+            else:
+                raise Exception("checkpoint_att is in incorrect format")
+
+    if print_att == None:
+        print_att = ("none", 0)
+    elif len(print_att) == 1:
+        if isinstance(print_att, str):
+            raise Exception("print_att is need a print_every more than where it print")
+        else:
+            print_att = ("epoch", print_att[0])
+
+    for iepoch in range(n_epoches):
+        shuffer_idx = torch.randperm(X.shape[0])
+        
+        model.train(True)
+        start = time.time()
+        for istart in range(0, X.shape[0], batch_size):
+            if print_att is not "batch":
+                print("\rEpoch[{:4d}/{}]\tPercentage= {:2.2f}%"
+                    .format(iepoch + 1, n_epoches, istart * 100 / X.shape[0]), end ="")
+
+            batch_X = X[shuffer_idx[istart: istart + batch_size]]
+            batch_Y = Y[shuffer_idx[istart: istart + batch_size]]
+
+            batch_O = model(batch_X)
+
+            loss = criterion(batch_O, batch_Y.view(batch_O.shape))
+            loss.backward()
+
+            optimizer.step()
+
+            if history_att == "batch":
+                hist_loss.append(loss.item())
+
+            if print_att[0] == "batch" and (istart + 1) % print_att[1]:
+                print("\rEpoch[{:4d}/{}]\tBatch[{:4d}/{}]\tLoss= {:8.6f}".format(
+                    iepoch + 1, n_epoches,
+                    istart, X.shape[0],
+                    loss.item(),
+                ))
+        
+        model.eval()
+        loss = criterion(model(X), Y.view(X.shape))
+
+        if history_att == "epoch":
+            hist_loss.append(loss.item())
+        
+        if print_att[0] == "batch" and (iepoch + 1) % print_att[1]:
+            print("\rEpoch[{:4d}/{}]\tLoss= {:8.6f}\tElapsed time= {:.2f}s".format(
+                iepoch + 1, n_epoches,
+                loss.item(),
+                time.time() - start
+            ))
+        
+        if (iepoch + 1) % checkpoint_att[0]:
+            t = datetime.datetime.now()
+            PATH = checkpoint_att[1] + "/{}-loss={:.4f}".format(t, loss.item())
+            torch.save(model.state_dict(), PATH)
+            print("Module was saved in {}".format(PATH))
+    all_end = time.time()
+    print("Elapsed time= {:.2f}s\tAvarage elapsed time per epoch= {:2f}s"
+        .format(all_end - all_start, (time.time() - all_start) / n_epoches))
+    return model, hist_loss
