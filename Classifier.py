4ff6687fa3d693a9eea365c5b4fb66c195f5cbbb
Le Huy <huykingsofm@gmail.com>
2019-07-10 20:57:08 +0700

    Delete FCNN, NNUtils, Add Module, BatchLoader

--- Classifier.py
+++ Classifier.py
@@ -8,5 +8,17 @@ class Classifier(Module):
         output = self(self.validating_set[0])
         output = torch.argmax(output, dim= 1)
 
-        n_true = (output == self.validating_set[1].view(output.shape)).sum().item()
-        return "Accuracy={:.2f}%".format(100 * n_true/self.validating_set[0].shape[0])
+        n_true = (output.type(self.validating_set[1].type()) == self.validating_set[1]).sum().item()
+        return "Accuracy={:.2f}%".format(100 * n_true / self.validating_set[0].shape[0])
+
+if __name__ == "__main__":
+    import torch.nn as nn
+    X = torch.Tensor([[1, 2], [2, 3], [4, 3]])
+    Y = torch.Tensor([2, 2, 2])
+    model = Classifier()
+    model.add_module("Dense1", nn.Linear(2, 3))
+    model.add_module("Softmax", nn.Softmax(dim=1))
+    print(model)
+    model.validating_set = (X, Y)
+    print(model(X))
+    print(model.__metric__())
